5b83b2647030420010001548f4285e3d6a9f5fa7
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-07-02 01:29:57 -0400

    add project.js to controllers

---
+++ controllers/project.js
@@ -0,0 +1,47 @@
+/* Step 1 import express
+ *
+ */
+const express = require('express')
+
+/* Step 2
+ *
+ * Import the api files from the models
+ *
+ * TODO: change the file path to the models file you'll need to use.
+ * TODO: rename this from `templateApi` to something more sensible (e.g:
+ * `shopsAPI`)
+ *
+ * NOTE: You may need to import more than one API to create the 
+ * controller you need.
+ * 
+ */
+const projectApi = require('../models/project.js')
+
+/* Step 3 
+ * 
+ * Create a new router.
+ *
+ * the router will "contain" all the request handlers that you define in this file.
+ * TODO: rename this from templateRouter to something that makes sense. (e.g:
+ * `shopRouter`)
+ */
+const projectRouter = express.Router()
+
+/* Step 4
+ * 
+ * TODO: Put all request handlers here
+ */
+
+/* Step 5
+ *
+  * TODO: delete this handler; it's just a sample
+ */
+//request handler to get new organization
+organizationRouter.get('/new', (req, res) => {
+    // res.render('projects/newProjectForm.hbs')
+  
+})
+
+module.exports = {
+  projectRouter
+}
