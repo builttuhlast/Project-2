f9b51642454695df4524ea321ec3592e2d81419e
Tiffany Mitchell <mitchelltiffany0@gmail.com>
2019-07-03 14:55:36 -0400

    add testData file to models

---
+++ models/testData.js
@@ -0,0 +1,16 @@
+const organizationApi = require('../models/organization.js')
+
+
+const projectApi = require('../models/connection.js')
+
+
+const bidApi = require('../models/connection.js')
+
+
+
+
+
+
+
+
+
